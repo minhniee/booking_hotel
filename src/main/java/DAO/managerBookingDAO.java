22f9fce604516dd2c648455dcd3be1d869f56c7c
minh <sktt1minh2003@gmail.com>
2024-07-21 23:31:11 +0700

    update  image

--- src/main/java/DAO/managerBookingDAO.java
+++ src/main/java/DAO/managerBookingDAO.java
@@ -1,34 +1,34 @@
 package DAO;
 
 import context.DBContext;
+import model.Booking;
 import model.ManagerBooking;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class managerBookingDAO extends DBContext {
-    public ArrayList<ManagerBooking> managerBookingList() {
-        ArrayList<ManagerBooking> ManagerBooking = new ArrayList<ManagerBooking>();
-        String sql = "  select id, room_id, account_id, checkin_date, checkout_date, num_child, num_adults, booking_price from booking\n";
-        try {
-            PreparedStatement stm =  getConnection().prepareStatement(sql);
-            ResultSet rs = stm.executeQuery();
-            while (rs.next()) {
-                ManagerBooking.add(new ManagerBooking(rs.getString(1)
+//    public ArrayList<ManagerBooking> managerBookingList() {
+//        ArrayList<ManagerBooking> ManagerBooking = new ArrayList<ManagerBooking>();
+//        String sql = "  select id, room_id, account_id, checkin_date, checkout_date, num_child, num_adults, booking_price from booking\n";
+//        try {
+//            PreparedStatement stm =  getConnection().prepareStatement(sql);
+//            ResultSet rs = stm.executeQuery();
+//            while (rs.next()) {
+//                ManagerBooking.add(new ManagerBooking(rs.getString(1)
+//
+//                        ,rs.getString(2)
+//                        ,rs.getString(3)
+//                        ,rs.getDate(4)
+//                        ,rs.getDate(5)
+//                        ,rs.getInt(6)
+//                        ,rs.getInt(7)
+//                        ,rs.getDouble(8)));
+//            }
+//        }catch (Exception e) {}
+//        return ManagerBooking;
+//    }
 
-                        ,rs.getString(2)
-                        ,rs.getString(3)
-                        ,rs.getDate(4)
-                        ,rs.getDate(5)
-                        ,rs.getInt(6)
-                        ,rs.getInt(7)
-                        ,rs.getDouble(8)));
-            }
-        }catch (Exception e) {}
-        return ManagerBooking;
-    }
     public ArrayList<ManagerBooking> getManagerBooking(String id) {
         ArrayList<ManagerBooking> list = new ArrayList<>();
         String sql = "  select id, room_id, account_id, checkin_date, checkout_date, num_child, num_adults, booking_price from booking where room_id like ?";
@@ -53,8 +53,26 @@ public class managerBookingDAO extends DBContext {
         return list;
     }
 
-    public static void main(String[] args) {
-        ArrayList<ManagerBooking> list = new managerBookingDAO().managerBookingList();
-        System.out.println(list);
+
+    public ArrayList<ManagerBooking> managerBookingList() throws SQLException {
+
+        ArrayList<ManagerBooking> bookings = new ArrayList<>();
+        String sql = "  select id, room_id, account_id, checkin_date, checkout_date, num_child, num_adults, booking_price from booking\n";
+        PreparedStatement pr = getConnection().prepareStatement(sql);
+        ResultSet rs = pr.executeQuery();
+        while (rs.next()) {
+             String id = rs.getString(1);
+             String   room_id = rs.getString(2);
+             String customer_id = rs.getString(3);
+             Date checkin_date  = rs.getDate(4) ;
+             Date checkout_date  = rs.getDate(5) ;
+             int num_child = rs.getInt(6);
+             int num_adults = rs.getInt(7);
+             double booking_price   = rs.getDouble(8);
+            ManagerBooking b = new ManagerBooking(id,room_id,customer_id,checkin_date,checkout_date,num_child,num_adults,booking_price);
+            bookings.add(b);
+        }
+        return bookings;
+
     }
 }
